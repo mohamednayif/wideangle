6c999854f4150616b35a94d6c87d1b391b62264e
Nayif <mohamednayif@gmail.com>
2025-10-11 14:50:31 +0400

    Configure for custom domain wideangle.ae

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <Router basename="/wideangle">
+    <Router>
       <div className="min-h-screen bg-white">
         <Header />
         <main className="pt-20">
