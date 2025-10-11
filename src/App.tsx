bc8f506bb35199314329efda9d7d16e1794e7ed1
Nayif <mohamednayif@gmail.com>
2025-10-11 13:40:43 +0400

    Fix TypeScript errors: Remove unused React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { motion } from 'framer-motion'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
